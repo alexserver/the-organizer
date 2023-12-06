bfb66a70bf72dc642983264075004081f6be75da
Alejandro Gomez <alexserverone@gmail.com>
2023-12-06 14:54:47 -0600

    adding the foundation for select organizations

---
+++ app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
@@ -0,0 +1,14 @@
+import { OrganizationList } from "@clerk/nextjs";
+
+export default function CreateOrganizationPage() {
+  return (
+    <div>
+      <OrganizationList
+        hidePersonal
+        afterSelectOrganizationUrl="/organization/:id"
+        afterCreateOrganizationUrl="/organization/:id"
+      />
+      ;
+    </div>
+  );
+}
