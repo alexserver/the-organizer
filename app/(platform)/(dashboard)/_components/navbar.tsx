f3f128d07c82f59e7432cb198766e310357cb33f
Alejandro Gomez <alexserverone@gmail.com>
2023-12-08 17:51:43 -0600

    comit checkpoint, we have an a deeper route layout to handle organization changes, when you visit a URL now we change the organization id programatically.

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -31,7 +31,7 @@ export const Navbar = () => {
           hidePersonal
           afterCreateOrganizationUrl="/organization/:id"
           afterLeaveOrganizationUrl="/select-org"
-          afterSelectOrganizationUrl="organization/:id"
+          afterSelectOrganizationUrl="/organization/:id"
           appearance={{
             elements: {
               rootBox: {
