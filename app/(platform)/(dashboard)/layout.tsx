e1921cba63fa5cb2d26fa12527c634ea2d42b4f3
Alejandro Gomez <alexserverone@gmail.com>
2023-12-09 02:12:41 -0600

    adding sidebar and nav-item components to render a sidebar for desktop view

--- app/(platform)/(dashboard)/layout.tsx
+++ app/(platform)/(dashboard)/layout.tsx
@@ -2,7 +2,7 @@ import { Navbar } from "./_components/navbar";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-screen bg-gradient-to-r from-purple-500 to-cyan-400">
+    <div className="h-screen ">
       <Navbar />
       {children}
     </div>
